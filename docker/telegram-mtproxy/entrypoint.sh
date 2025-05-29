11f75f3d6682d96833f441e4945a825aa3d9856c
kienlt <kienlt@topcv.vn>
2025-05-29 16:36:38 +0700

    feat: rm telegram-mtproxy

--- docker/telegram-mtproxy/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-adduser --home /home/${ACCESS_KEY} --disabled-password --shell /bin/bash --gecos GECOS ${ACCESS_KEY}
-
-mtproto-proxy --slaves 1 --user ${ACCESS_KEY} -p 8080 -H 8443 --mtproto-secret ${SECRET_KEY} --aes-pwd /proxy-secret /proxy-multi.conf
