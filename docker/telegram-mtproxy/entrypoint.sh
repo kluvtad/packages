cde497ff7d8d0c5fb0bbb506dfef69041b04e196
kienlt <kienlt@topcv.vn>
2025-05-29 16:22:56 +0700

    feat(tele-proxy): update port

--- docker/telegram-mtproxy/entrypoint.sh
+++ docker/telegram-mtproxy/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 adduser --home /home/${ACCESS_KEY} --disabled-password --shell /bin/bash --gecos GECOS ${ACCESS_KEY}
 
-mtproto-proxy --port 8443 --slaves 1 --user ${ACCESS_KEY} --http-ports 8080 --mtproto-secret ${SECRET_KEY} --aes-pwd /proxy-secret /proxy-multi.conf
+mtproto-proxy --slaves 1 --user ${ACCESS_KEY} -p 8080 -H 8443 --mtproto-secret ${SECRET_KEY} --aes-pwd /proxy-secret /proxy-multi.conf
