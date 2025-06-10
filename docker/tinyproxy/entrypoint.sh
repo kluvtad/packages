b27934f8743ceff7ac98661c91cab5f5fcd90a22
kienlt <kienlt@topcv.vn>
2025-06-10 15:01:11 +0700

    feat(docker/tinyproxy): check TINYPROXY_USER

--- docker/tinyproxy/entrypoint.sh
+++ docker/tinyproxy/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 cp /etc/tinyproxy/tinyproxy.conf /workspace/tinyproxy.conf
-if [ -n "$TINYPROXY_PASSWORD" ]; then
+if [ -n "$TINYPROXY_USER"] && [ -n "$TINYPROXY_PASSWORD" ]; then
     echo "BasicAuth $TINYPROXY_USER $TINYPROXY_PASSWORD" >> /workspace/tinyproxy.conf
 fi 
 tinyproxy -d -c /workspace/tinyproxy.conf
