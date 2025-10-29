61e365a4ce279e792b6061b42a9ab0ba8c0f7c8b
kevinlee <kien.letrung610@gmail.com>
2025-10-29 11:26:44 +0700

    docs(docker/postgresql-setup): dev setup

---
+++ docker/postgresql-setup/README.md
@@ -0,0 +1,17 @@
+## Local dev
+1. Create `.env`
+```properties
+MASTER_PGHOST=localhost
+MASTER_PGPORT=5432
+MASTER_PGDATABASE=postgres
+MASTER_PGUSER=postgres
+MASTER_PGPASSWORD=...
+```
+2. Build docker
+```bash
+$ docker build -t test .
+```
+3. Run for dev
+```bash
+$ docker run -it --network=host --env-file=.env -v $PWD:/workspace --entrypoint sh test
+```
--- docker/postgresql-setup/entrypoint.sh
+++ docker/postgresql-setup/entrypoint.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
+if [ -z "$MASTER_PGHOST" ] || [ -z "$MASTER_PGDATABASE" ] || [ -z "$MASTER_PGUSER" ] || [ -z "$MASTER_PGPASSWORD" ] ; then
+  echo "Error: MASTER_PGHOST, MASTER_PGDATABASE, MASTER_PGUSER, and MASTER_PGPASSWORD environment variables must be set."
+  exit 1
+fi
+
+export MASTER_PGPORT=${MASTER_PGPORT:-5432}
+
 ansible-playbook -i ./inventory/hosts.ini ./playbook.yml
